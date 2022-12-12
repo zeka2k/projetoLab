ad0d43b8ce21aada0027de9d0230894b03314af1
LeDuude <jose.silva.11@hotmail.com>
2022-12-12 13:55:28 +0000

    auth/login

---
+++ resources/views/adminHome.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+                <div class="card-body">
+                    You are Admin.
+                </div>
+
+                <div class="card-body">
+                    @if ($message = Session::get('status'))
+                    <div class="alert alert-success">
+                        <p>{{ $message }}</p>
+                    </div>
+                    @endif
+                    @include('partials.upload')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\RoutingController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +18,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 //-----> Parecido com controlers mais simples para mais complexos fazer no controlador
-Route::get('/', [RoutingController::class, 'index']);
-Route::get('/about', [RoutingController::class, 'about']);
+//Route::get('/', [RoutingController::class, 'index']);
+//Route::get('/about', [RoutingController::class, 'about']);
+
+
+Route::get('/', function () {
+  return view('welcome');
+});
 
 
 Route::get('/clients', [ClientController::class, 'index']);
@@ -28,3 +35,8 @@ Route::get('/clients/show/{client}', [ClientController::class, 'show']);
 Route::get('/clients/edit/{client}', [ClientController::class, 'edit']);
 Route::post('/clients/update/{client}', [ClientController::class, 'update']);
 Route::post('/clients/destoy/{client}', [ClientController::class, 'destroy']);
+
+Auth::routes();
+//Auth::routes(['verify' => true]);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
